45805cf5034f127e7bd2dbd81a390d2c037cea33
Caleb Bassi <calebjbassi@gmail.com>
2020-08-31 08:51:28 -0700

    convert to tabs

--- src/lib.rs
+++ src/lib.rs
@@ -22,323 +22,323 @@ pub use error::*;
 use utils::{move_file, AbsolutePath};
 
 pub struct Trash {
-    home_trash: PathBuf,
+	home_trash: PathBuf,
 }
 
 impl Trash {
-    // TODO: errors
-    pub fn new() -> io::Result<Trash> {
-        let home_trash = AppDirs::new::<PathBuf>(None, AppUI::CommandLine)
-            .unwrap()
-            .data_dir
-            .join("Trash");
-        fs::create_dir_all(home_trash.join("files"))?;
-        fs::create_dir_all(home_trash.join("info"))?;
-        Ok(Trash { home_trash })
-    }
+	// TODO: errors
+	pub fn new() -> io::Result<Trash> {
+		let home_trash = AppDirs::new::<PathBuf>(None, AppUI::CommandLine)
+			.unwrap()
+			.data_dir
+			.join("Trash");
+		fs::create_dir_all(home_trash.join("files"))?;
+		fs::create_dir_all(home_trash.join("info"))?;
+		Ok(Trash { home_trash })
+	}
 
-    pub fn get_trashed_files(&self) -> Result<Vec<Result<TrashEntry>>> {
-        let results = self
-            .home_trash
-            .join("info")
-            .read_dir()
-            .context(ReadTrashInfoDir)?
-            .map(|dir_entry| {
-                let trash_info_path = dir_entry.context(ReadTrashInfoDir)?.path();
-                let trashed_path =
-                    self.home_trash
-                        .join("files")
-                        .join(trash_info_path.file_stem().context({
-                            InvalidTrashInfoPath {
-                                path: &trash_info_path,
-                            }
-                        })?);
-                let trash_info = fs::read_to_string(&trash_info_path)
-                    .context(ReadTrashInfo {
-                        path: &trash_info_path,
-                    })?
-                    .parse::<TrashInfo>()
-                    .context(ParseTrashInfo {
-                        path: &trash_info_path,
-                    })?;
-                Ok(TrashEntry {
-                    trashed_path,
-                    trash_info,
-                })
-            })
-            // TODO: what is this?
-            .sorted_by(|result1, result2| match result1 {
-                Ok(x) => match result2 {
-                    Ok(y) => Ord::cmp(&x.trash_info.deletion_date, &y.trash_info.deletion_date),
-                    Err(_) => Ordering::Less,
-                },
-                Err(_) => Ordering::Less,
-            })
-            .collect();
+	pub fn get_trashed_files(&self) -> Result<Vec<Result<TrashEntry>>> {
+		let results = self
+			.home_trash
+			.join("info")
+			.read_dir()
+			.context(ReadTrashInfoDir)?
+			.map(|dir_entry| {
+				let trash_info_path = dir_entry.context(ReadTrashInfoDir)?.path();
+				let trashed_path =
+					self.home_trash
+						.join("files")
+						.join(trash_info_path.file_stem().context({
+							InvalidTrashInfoPath {
+								path: &trash_info_path,
+							}
+						})?);
+				let trash_info = fs::read_to_string(&trash_info_path)
+					.context(ReadTrashInfo {
+						path: &trash_info_path,
+					})?
+					.parse::<TrashInfo>()
+					.context(ParseTrashInfo {
+						path: &trash_info_path,
+					})?;
+				Ok(TrashEntry {
+					trashed_path,
+					trash_info,
+				})
+			})
+			// TODO: what is this?
+			.sorted_by(|result1, result2| match result1 {
+				Ok(x) => match result2 {
+					Ok(y) => Ord::cmp(&x.trash_info.deletion_date, &y.trash_info.deletion_date),
+					Err(_) => Ordering::Less,
+				},
+				Err(_) => Ordering::Less,
+			})
+			.collect();
 
-        Ok(results)
-    }
+		Ok(results)
+	}
 
-    pub fn trash_file<P>(&self, path: P) -> Result<PathBuf>
-    where
-        P: AsRef<Path>,
-    {
-        let path = path.as_ref();
-        let path = path.absolute_path().map_err(|_| Error::InvalidPath {
-            path: path.to_path_buf(),
-        })?;
-        ensure!(path.exists(), InvalidPath { path });
+	pub fn trash_file<P>(&self, path: P) -> Result<PathBuf>
+	where
+		P: AsRef<Path>,
+	{
+		let path = path.as_ref();
+		let path = path.absolute_path().map_err(|_| Error::InvalidPath {
+			path: path.to_path_buf(),
+		})?;
+		ensure!(path.exists(), InvalidPath { path });
 
-        // check if given file contains the trash-can
-        ensure!(
-            !self.home_trash.starts_with(&path),
-            TrashingTrashCan { path }
-        );
+		// check if given file contains the trash-can
+		ensure!(
+			!self.home_trash.starts_with(&path),
+			TrashingTrashCan { path }
+		);
 
-        let trashed_path = move_file(
-            &path,
-            &self
-                .home_trash
-                .join("files")
-                .join(path.file_name().expect("path is clean")),
-        )
-        .context(MoveFile { path: &path })?;
+		let trashed_path = move_file(
+			&path,
+			&self
+				.home_trash
+				.join("files")
+				.join(path.file_name().expect("path is clean")),
+		)
+		.context(MoveFile { path: &path })?;
 
-        let trash_info_path = get_trash_info_path(&self.home_trash, &trashed_path);
-        let trash_info = TrashInfo {
-            original_path: path.to_path_buf(),
-            deletion_date: Local::now(),
-        };
-        fs::write(trash_info_path, format!("{}\n", trash_info)).context(WriteTrashInfo { path })?;
+		let trash_info_path = get_trash_info_path(&self.home_trash, &trashed_path);
+		let trash_info = TrashInfo {
+			original_path: path.to_path_buf(),
+			deletion_date: Local::now(),
+		};
+		fs::write(trash_info_path, format!("{}\n", trash_info)).context(WriteTrashInfo { path })?;
 
-        Ok(trashed_path)
-    }
+		Ok(trashed_path)
+	}
 
-    pub fn restore_trashed_file<P>(&self, path: P) -> Result<PathBuf>
-    where
-        P: AsRef<Path>,
-    {
-        let path = path.as_ref();
-        let path = path.absolute_path().map_err(|_| Error::InvalidPath {
-            path: path.to_path_buf(),
-        })?;
-        ensure!(path.exists(), InvalidPath { path });
+	pub fn restore_trashed_file<P>(&self, path: P) -> Result<PathBuf>
+	where
+		P: AsRef<Path>,
+	{
+		let path = path.as_ref();
+		let path = path.absolute_path().map_err(|_| Error::InvalidPath {
+			path: path.to_path_buf(),
+		})?;
+		ensure!(path.exists(), InvalidPath { path });
 
-        let trash_info_path = get_trash_info_path(&self.home_trash, &path);
-        let original_path = fs::read_to_string(&trash_info_path)
-            .context(ReadTrashInfo {
-                path: &trash_info_path,
-            })?
-            .parse::<TrashInfo>()
-            .context(ParseTrashInfo { path: &path })?
-            .original_path;
-        let restored_path = move_file(&path, &original_path).context(MoveFile { path: &path })?;
-        fs::remove_file(trash_info_path).context(RemoveFile { path })?;
+		let trash_info_path = get_trash_info_path(&self.home_trash, &path);
+		let original_path = fs::read_to_string(&trash_info_path)
+			.context(ReadTrashInfo {
+				path: &trash_info_path,
+			})?
+			.parse::<TrashInfo>()
+			.context(ParseTrashInfo { path: &path })?
+			.original_path;
+		let restored_path = move_file(&path, &original_path).context(MoveFile { path: &path })?;
+		fs::remove_file(trash_info_path).context(RemoveFile { path })?;
 
-        Ok(restored_path)
-    }
+		Ok(restored_path)
+	}
 
-    pub fn erase_file<P>(&self, path: P) -> Result<()>
-    where
-        P: AsRef<Path>,
-    {
-        let path = path.as_ref();
-        let path = path.absolute_path().map_err(|_| Error::InvalidPath {
-            path: path.to_path_buf(),
-        })?;
-        ensure!(path.exists(), InvalidPath { path });
+	pub fn erase_file<P>(&self, path: P) -> Result<()>
+	where
+		P: AsRef<Path>,
+	{
+		let path = path.as_ref();
+		let path = path.absolute_path().map_err(|_| Error::InvalidPath {
+			path: path.to_path_buf(),
+		})?;
+		ensure!(path.exists(), InvalidPath { path });
 
-        if self.is_file_trashed(&path).expect("file exists") {
-            fs::remove_file(get_trash_info_path(&self.home_trash, &path))
-                .context(RemoveFile { path: &path })?;
-        }
-        if path.is_dir() {
-            fs::remove_dir_all(&path).context(RemoveFile { path: &path })?;
-        } else {
-            fs::remove_file(&path).context(RemoveFile { path: &path })?;
-        }
+		if self.is_file_trashed(&path).expect("file exists") {
+			fs::remove_file(get_trash_info_path(&self.home_trash, &path))
+				.context(RemoveFile { path: &path })?;
+		}
+		if path.is_dir() {
+			fs::remove_dir_all(&path).context(RemoveFile { path: &path })?;
+		} else {
+			fs::remove_file(&path).context(RemoveFile { path: &path })?;
+		}
 
-        Ok(())
-    }
+		Ok(())
+	}
 
-    pub fn is_file_trashed<P>(&self, path: P) -> Result<bool>
-    where
-        P: AsRef<Path>,
-    {
-        let path = path.as_ref();
-        let path = path.absolute_path().map_err(|_| Error::InvalidPath {
-            path: path.to_path_buf(),
-        })?;
-        ensure!(path.exists(), InvalidPath { path });
+	pub fn is_file_trashed<P>(&self, path: P) -> Result<bool>
+	where
+		P: AsRef<Path>,
+	{
+		let path = path.as_ref();
+		let path = path.absolute_path().map_err(|_| Error::InvalidPath {
+			path: path.to_path_buf(),
+		})?;
+		ensure!(path.exists(), InvalidPath { path });
 
-        Ok(path.starts_with(&self.home_trash.join("files")))
-    }
+		Ok(path.starts_with(&self.home_trash.join("files")))
+	}
 }
 
 fn get_trash_info_path(trash_path: &Path, file: &Path) -> PathBuf {
-    trash_path.join("info").join(format!(
-        "{}.trashinfo",
-        file.file_name().expect("path is clean").to_string_lossy()
-    ))
+	trash_path.join("info").join(format!(
+		"{}.trashinfo",
+		file.file_name().expect("path is clean").to_string_lossy()
+	))
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct TrashEntry {
-    pub trashed_path: PathBuf,
-    pub trash_info: TrashInfo,
+	pub trashed_path: PathBuf,
+	pub trash_info: TrashInfo,
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct TrashInfo {
-    pub original_path: PathBuf,
-    pub deletion_date: DateTime<Local>,
+	pub original_path: PathBuf,
+	pub deletion_date: DateTime<Local>,
 }
 
 fn parse_trash_info<'a>(input: &'a str) -> IResult<&'a str, TrashInfo, VerboseError<&'a str>> {
-    let (input, _) = tag("[Trash Info]\n")(input)?;
+	let (input, _) = tag("[Trash Info]\n")(input)?;
 
-    let (input, _) = tag("Path=")(input)?;
-    let (input, original_path) = map_res(take_until("\n"), |input| {
-        let path = PathBuf::from(input);
-        if path.is_relative() || path.parent().is_none() || path != path.clean() {
-            Err(io::Error::new(io::ErrorKind::InvalidData, "invalid path"))
-        } else {
-            Ok(path)
-        }
-    })(input)?;
-    let (input, _) = tag("\n")(input)?;
+	let (input, _) = tag("Path=")(input)?;
+	let (input, original_path) = map_res(take_until("\n"), |input| {
+		let path = PathBuf::from(input);
+		if path.is_relative() || path.parent().is_none() || path != path.clean() {
+			Err(io::Error::new(io::ErrorKind::InvalidData, "invalid path"))
+		} else {
+			Ok(path)
+		}
+	})(input)?;
+	let (input, _) = tag("\n")(input)?;
 
-    let (input, _) = tag("DeletionDate=")(input)?;
-    let (input, deletion_date) = map_res(take_until("\n"), |input| {
-        Local.datetime_from_str(input, "%Y-%m-%dT%H:%M:%S")
-    })(input)?;
+	let (input, _) = tag("DeletionDate=")(input)?;
+	let (input, deletion_date) = map_res(take_until("\n"), |input| {
+		Local.datetime_from_str(input, "%Y-%m-%dT%H:%M:%S")
+	})(input)?;
 
-    Ok((
-        input,
-        TrashInfo {
-            original_path,
-            deletion_date,
-        },
-    ))
+	Ok((
+		input,
+		TrashInfo {
+			original_path,
+			deletion_date,
+		},
+	))
 }
 
 // TODO
 impl FromStr for TrashInfo {
-    type Err = io::Error;
+	type Err = io::Error;
 
-    fn from_str(s: &str) -> io::Result<Self> {
-        parse_trash_info(s).map(|x| x.1).map_err(|_| {
-            // TODO figure out how to convert nom::error to failure::error while preserving its error message.
-            // Was having issues since failure::error requires a static lifetime and nom::error contains a &str.
-            io::Error::new(io::ErrorKind::InvalidData, "failed to parse TrashInfo").into()
-        })
-    }
+	fn from_str(s: &str) -> io::Result<Self> {
+		parse_trash_info(s).map(|x| x.1).map_err(|_| {
+			// TODO figure out how to convert nom::error to failure::error while preserving its error message.
+			// Was having issues since failure::error requires a static lifetime and nom::error contains a &str.
+			io::Error::new(io::ErrorKind::InvalidData, "failed to parse TrashInfo").into()
+		})
+	}
 }
 
 impl fmt::Display for TrashInfo {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "[Trash Info]\nPath={}\nDeletionDate={}",
-            self.original_path.display(),
-            self.deletion_date.format("%Y-%m-%dT%H:%M:%S"),
-        )
-    }
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(
+			f,
+			"[Trash Info]\nPath={}\nDeletionDate={}",
+			self.original_path.display(),
+			self.deletion_date.format("%Y-%m-%dT%H:%M:%S"),
+		)
+	}
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn test_get_trashed_files() {
-        use std::fs::File;
+	#[test]
+	fn test_get_trashed_files() {
+		use std::fs::File;
 
-        let trash = Trash {
-            home_trash: PathBuf::from("test2"),
-        };
-        fs::create_dir_all(&trash.home_trash.join("files"));
-        fs::create_dir_all(&trash.home_trash.join("info"));
-        let trash_info = TrashInfo {
-            original_path: PathBuf::from("/asdf/123"),
-            deletion_date: Local.ymd(2014, 7, 8).and_hms(9, 10, 11),
-        };
+		let trash = Trash {
+			home_trash: PathBuf::from("test2"),
+		};
+		fs::create_dir_all(&trash.home_trash.join("files"));
+		fs::create_dir_all(&trash.home_trash.join("info"));
+		let trash_info = TrashInfo {
+			original_path: PathBuf::from("/asdf/123"),
+			deletion_date: Local.ymd(2014, 7, 8).and_hms(9, 10, 11),
+		};
 
-        fs::remove_dir_all(trash.home_trash).unwrap();
-    }
+		fs::remove_dir_all(trash.home_trash).unwrap();
+	}
 
-    #[test]
-    fn test_trash_file() {}
+	#[test]
+	fn test_trash_file() {}
 
-    #[test]
-    fn test_restore_trashed_file() {}
+	#[test]
+	fn test_restore_trashed_file() {}
 
-    #[test]
-    fn test_erase_file() {
-        use std::fs::File;
+	#[test]
+	fn test_erase_file() {
+		use std::fs::File;
 
-        let trash = Trash {
-            home_trash: PathBuf::from("test"),
-        };
-        let files_dir = trash.home_trash.join("files");
-        let info_dir = trash.home_trash.join("info");
-        let in_trash = files_dir.join("in_trash");
-        let in_trash_trash_info = info_dir.join("in_trash.trashinfo");
+		let trash = Trash {
+			home_trash: PathBuf::from("test"),
+		};
+		let files_dir = trash.home_trash.join("files");
+		let info_dir = trash.home_trash.join("info");
+		let in_trash = files_dir.join("in_trash");
+		let in_trash_trash_info = info_dir.join("in_trash.trashinfo");
 
-        fs::create_dir_all(&files_dir);
-        fs::create_dir_all(&info_dir);
+		fs::create_dir_all(&files_dir);
+		fs::create_dir_all(&info_dir);
 
-        assert!(&files_dir.exists());
-        assert!(&info_dir.exists());
+		assert!(&files_dir.exists());
+		assert!(&info_dir.exists());
 
-        File::create(&in_trash);
-        File::create(&in_trash_trash_info);
+		File::create(&in_trash);
+		File::create(&in_trash_trash_info);
 
-        assert!((&in_trash).exists());
-        assert!((&in_trash_trash_info).exists());
+		assert!((&in_trash).exists());
+		assert!((&in_trash_trash_info).exists());
 
-        trash.erase_file(&in_trash);
+		trash.erase_file(&in_trash);
 
-        assert!(!(&in_trash).exists());
-        assert!(!(&in_trash_trash_info).exists());
+		assert!(!(&in_trash).exists());
+		assert!(!(&in_trash_trash_info).exists());
 
-        let out_trash = trash.home_trash.join("asdf");
-        File::create(&out_trash);
-        assert!(&out_trash.exists());
-        trash.erase_file(&out_trash);
-        assert!(!&out_trash.exists());
+		let out_trash = trash.home_trash.join("asdf");
+		File::create(&out_trash);
+		assert!(&out_trash.exists());
+		trash.erase_file(&out_trash);
+		assert!(!&out_trash.exists());
 
-        fs::remove_dir_all(trash.home_trash).unwrap();
-    }
+		fs::remove_dir_all(trash.home_trash).unwrap();
+	}
 
-    #[test]
-    fn test_is_file_trashed() {
-        let trash = Trash {
-            home_trash: PathBuf::from("/test/trash"),
-        };
-        let file1 = PathBuf::from("/test/trash/files/foo");
-        let file2 = PathBuf::from("/test/trash/info/foo");
-        assert!(trash.is_file_trashed(file1).unwrap());
-        assert!(!trash.is_file_trashed(file2).unwrap());
-    }
+	#[test]
+	fn test_is_file_trashed() {
+		let trash = Trash {
+			home_trash: PathBuf::from("/test/trash"),
+		};
+		let file1 = PathBuf::from("/test/trash/files/foo");
+		let file2 = PathBuf::from("/test/trash/info/foo");
+		assert!(trash.is_file_trashed(file1).unwrap());
+		assert!(!trash.is_file_trashed(file2).unwrap());
+	}
 
-    #[test]
-    fn test_trash_info_parsing() {
-        let trash_info = TrashInfo {
-            original_path: PathBuf::from("/asdf/123"),
-            deletion_date: Local.ymd(2014, 7, 8).and_hms(9, 10, 11),
-        };
-        let trash_info_to_str = "[Trash Info]\nPath=/asdf/123\nDeletionDate=2014-07-08T09:10:11";
-        assert_eq!(trash_info, trash_info_to_str.parse::<TrashInfo>().unwrap());
-    }
+	#[test]
+	fn test_trash_info_parsing() {
+		let trash_info = TrashInfo {
+			original_path: PathBuf::from("/asdf/123"),
+			deletion_date: Local.ymd(2014, 7, 8).and_hms(9, 10, 11),
+		};
+		let trash_info_to_str = "[Trash Info]\nPath=/asdf/123\nDeletionDate=2014-07-08T09:10:11";
+		assert_eq!(trash_info, trash_info_to_str.parse::<TrashInfo>().unwrap());
+	}
 
-    #[test]
-    fn test_trash_info_display() {
-        let trash_info = TrashInfo {
-            original_path: PathBuf::from("/asdf/123"),
-            deletion_date: Local.ymd(2014, 7, 8).and_hms(9, 10, 11),
-        };
-        let trash_info_to_str = "[Trash Info]\nPath=/asdf/123\nDeletionDate=2014-07-08T09:10:11";
-        assert_eq!(trash_info.to_string(), trash_info_to_str);
-    }
+	#[test]
+	fn test_trash_info_display() {
+		let trash_info = TrashInfo {
+			original_path: PathBuf::from("/asdf/123"),
+			deletion_date: Local.ymd(2014, 7, 8).and_hms(9, 10, 11),
+		};
+		let trash_info_to_str = "[Trash Info]\nPath=/asdf/123\nDeletionDate=2014-07-08T09:10:11";
+		assert_eq!(trash_info.to_string(), trash_info_to_str);
+	}
 }
--- src/utils.rs
+++ src/utils.rs
@@ -11,25 +11,25 @@ use path_clean::PathClean;
 pub fn move_file(from: &Path, to: &Path) -> io::Result<PathBuf>
 where
 {
-    let to_dir = to.parent().expect("path is valid destination").to_owned();
-    let to_filename = to
-        .file_name()
-        .expect("path is clean")
-        .to_string_lossy()
-        .to_string();
+	let to_dir = to.parent().expect("path is valid destination").to_owned();
+	let to_filename = to
+		.file_name()
+		.expect("path is clean")
+		.to_string_lossy()
+		.to_string();
 
-    fs::create_dir_all(&to_dir)?;
+	fs::create_dir_all(&to_dir)?;
 
-    let mut count = 1;
-    let mut to = to.to_path_buf();
-    while to.exists() {
-        to = to.with_file_name(format!("{}_{}", to_filename, count));
-        count += 1;
-    }
+	let mut count = 1;
+	let mut to = to.to_path_buf();
+	while to.exists() {
+		to = to.with_file_name(format!("{}_{}", to_filename, count));
+		count += 1;
+	}
 
-    fs::rename(&from, &to)?;
+	fs::rename(&from, &to)?;
 
-    Ok(to)
+	Ok(to)
 }
 
 /// Propogates errors from `std::env::current_dir`.
@@ -37,67 +37,67 @@ where
 /// Example: `PathBuf::from(".").file_name() == None`, `PathBuf::from(".").starts_with("/") == False`
 pub fn absolute_path<P>(path: P) -> io::Result<PathBuf>
 where
-    P: AsRef<Path>,
+	P: AsRef<Path>,
 {
-    let path = path.as_ref();
-    let pathbuf = if path.is_absolute() {
-        path.to_path_buf()
-    } else {
-        if path == PathBuf::from("") {
-            return Err(io::Error::new(
-                io::ErrorKind::InvalidInput,
-                "path cannot be empty",
-            ));
-        } else {
-            env::current_dir()?.join(path)
-        }
-    }
-    // cleans up the path since we don't want something like `/foo/bar/..`
-    .clean();
+	let path = path.as_ref();
+	let pathbuf = if path.is_absolute() {
+		path.to_path_buf()
+	} else {
+		if path == PathBuf::from("") {
+			return Err(io::Error::new(
+				io::ErrorKind::InvalidInput,
+				"path cannot be empty",
+			));
+		} else {
+			env::current_dir()?.join(path)
+		}
+	}
+	// cleans up the path since we don't want something like `/foo/bar/..`
+	.clean();
 
-    Ok(pathbuf)
+	Ok(pathbuf)
 }
 
 pub trait AbsolutePath {
-    fn absolute_path(&self) -> io::Result<PathBuf>;
+	fn absolute_path(&self) -> io::Result<PathBuf>;
 }
 
 impl AbsolutePath for PathBuf {
-    fn absolute_path(&self) -> io::Result<PathBuf> {
-        absolute_path(&self)
-    }
+	fn absolute_path(&self) -> io::Result<PathBuf> {
+		absolute_path(&self)
+	}
 }
 
 impl AbsolutePath for Path {
-    fn absolute_path(&self) -> io::Result<PathBuf> {
-        absolute_path(&self)
-    }
+	fn absolute_path(&self) -> io::Result<PathBuf> {
+		absolute_path(&self)
+	}
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn test_rename_file_handle_conflicts() {
-        use std::fs::File;
+	#[test]
+	fn test_rename_file_handle_conflicts() {
+		use std::fs::File;
 
-        let folder = PathBuf::from("test1");
-        fs::create_dir_all(&folder);
-        let file = folder.join("file");
-        let file1 = folder.join("foo");
-        File::create(&file);
-        File::create(&file1);
-        assert!(&file.exists());
-        assert_eq!(move_file(&file, &file1).unwrap(), folder.join("foo_1"));
-        assert!(!&file.exists());
-        File::create(&file);
-        assert!(move_file(&file, &file.join("asdf")).is_err());
-        assert_eq!(
-            move_file(&file, &PathBuf::from(format!("{}asdf", file.display(),))).unwrap(),
-            folder.join("fileasdf")
-        );
+		let folder = PathBuf::from("test1");
+		fs::create_dir_all(&folder);
+		let file = folder.join("file");
+		let file1 = folder.join("foo");
+		File::create(&file);
+		File::create(&file1);
+		assert!(&file.exists());
+		assert_eq!(move_file(&file, &file1).unwrap(), folder.join("foo_1"));
+		assert!(!&file.exists());
+		File::create(&file);
+		assert!(move_file(&file, &file.join("asdf")).is_err());
+		assert_eq!(
+			move_file(&file, &PathBuf::from(format!("{}asdf", file.display(),))).unwrap(),
+			folder.join("fileasdf")
+		);
 
-        fs::remove_dir_all(folder).unwrap();
-    }
+		fs::remove_dir_all(folder).unwrap();
+	}
 }
